41d3b6dbf5b80e660876d3b0e15aca1494ca33d1
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2026-09-07 19:11:09 -0400

    feat!: Cleanup (#5)
    * feat!: Remove simple alias
    
    * chore: Remove print line per message

--- src/app.rs
+++ src/app.rs
@@ -103,27 +103,12 @@ pub trait App: Sized + 'static {
         }
     }
 
-    /// Alias for [`App::process_signal`].
-    ///
-    /// # Errors
-    ///
-    /// Returns an error if effect evaluation fails.
-    fn process_effect(&mut self, effect: Signal<Self::Message, ()>) -> anyhow::Result<Task<Self::Message>> {
-        self.process_signal(effect)
-    }
-
     /// Dispatches a message through [`App::update`] and evaluates the resulting [`Signal`].
-    ///
-    /// In debug builds, prints the received message.
     ///
     /// # Errors
     ///
     /// Returns an error if the update or resulting signal execution fails.
     fn process_message(&mut self, message: Self::Message) -> anyhow::Result<Task<Self::Message>> {
-        #[cfg(debug_assertions)]
-        {
-            println!("Msg: {message:?}");
-        }
         let signal = self.update(message)?;
         self.process_signal(signal)
     }
